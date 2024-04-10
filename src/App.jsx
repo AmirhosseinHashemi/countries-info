653c30d0b68c36926777cdea6d10f76b3fadb109
Amirhossein <4mirh03einhashemi@gmail.com>
2024-04-11 01:42:19 +0330

    ➕🛠 add and set tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="text-2xl text-blue-600 tracking-widest font-bold">
+      Hello World
+    </h1>
+  );
 }
 
 export default App;
