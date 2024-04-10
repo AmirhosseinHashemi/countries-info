a6a5aa3252ef620704cf95f00ade9a6fc824b354
Amirhossein <4mirh03einhashemi@gmail.com>
2024-04-11 01:58:47 +0330

    🔧 add font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Nunito Sans, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
