ea5ba25ca5b88b019a77ebf0659cae4ae411cb69
Amirhossein <4mirh03einhashemi@gmail.com>
2024-04-19 22:24:24 +0330

    🐛 fix bug related style

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 function Container({ children }) {
   return (
-    <div className="m-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
+    <div className="m-auto overflow-x-hidden sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl">
       {children}
     </div>
   );
